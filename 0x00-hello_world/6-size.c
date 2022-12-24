6c5c32c7720bd7a6d02d3a33f941f6d3e236d410
esamjamma <esam_jammaa@outlook.com>
2022-12-24 01:58:08 -0800

    Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,2 @@
 #!/bin/bash
-printf("Size of a char: %i byte(s)\n", sizeof(char));
-        printf("Size of a int: %i byte(s)\n", sizeof(int));
-        printf("Size of a long int: %i byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %i byte(s)\n", sizeof(float));
-        return (0);
+gcc -S -masm=intel $CFILE -o ${CFILE//'.c'/'.s'}
