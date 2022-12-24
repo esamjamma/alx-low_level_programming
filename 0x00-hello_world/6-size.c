0245af62215ba4e8191b03ecbb0a7605e811b23e
esamjamma <esam_jammaa@outlook.com>
2022-12-24 02:06:19 -0800

    Tasks

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE -o ${CFILE//'.c'/'.s'}
+#include <stdio.h>
+int main(void)
+{
+    printf("char: %ld bytes\n", sizeof(char));
+    printf("short: %ld bytes\n", sizeof(short));
+    printf("int: %ld bytes\n", sizeof(int));
+    printf("long: %ld bytes\n", sizeof(long));
+    printf("long long: %ld bytes\n", sizeof(long long));
+    printf("float: %ld bytes\n", sizeof(float));
+    printf("double: %ld bytes\n", sizeof(double));
+    printf("long double: %ld bytes\n", sizeof(long double));
+
+    return 0;
+}
